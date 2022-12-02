9d4a914773c8e11b2d5c0ce912789d62c09538f6
Deekshith Yamjala <117278967+Y-Deekshith@users.noreply.github.com>
2022-12-02 22:39:09 +0530

    data_add.sh

--- data_add.sh
+++ data_add.sh
@@ -1,8 +1,7 @@
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo chmod 666 /var/run/docker.sock
-docker pull dhruvin30/dhsoniweb:v1
-docker run -d -p 80:80 dhruvin30/dhsoniweb:latest
+#! bin/bash
+yum install httpd -y
+service httpd start
+chkconfig httpd on
+cd /var/www/html
+touch index.html
+echo "<h1> Deekshith Yamjala </h1> > /var/www/html/index.html
